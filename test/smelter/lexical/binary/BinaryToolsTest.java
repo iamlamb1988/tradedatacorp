706d983d2e7b840df59b052caf664343bf8786fb
root <iamlamb1988@gmail.com>
2025-01-12 05:03:05 +0000

    Adding junit tests to begin testing all functions.

---
+++ test/smelter/lexical/binary/BinaryToolsTest.java
@@ -0,0 +1,11 @@
+package tradedatacorp.smelter.lexical.binary;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class BinaryToolsTest{
+    @Test
+    public void test(){
+        assertEquals(0,0);
+    }
+}
