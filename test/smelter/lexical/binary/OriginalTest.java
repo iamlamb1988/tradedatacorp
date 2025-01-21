f208c7b83b1da70523bc9fc0886a30737ba7e0a9
iamlamb1988 <iamlamb1988@gmail.com>
2025-01-21 00:01:07 +0000

    Added empty new file with dummy test to begin testing constructor.

---
+++ test/smelter/lexical/binary/OriginalTest.java
@@ -0,0 +1,12 @@
+package tradedatacorp.smelter.lexical.binary;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class OriginalTest{
+    @Test
+    public void initialTest(){
+        assertTrue(true);
+    }
+}
