25136c1b7f22fdcca0e19077a3b90144827d42fa
iamlamb1988 <iamlamb1988@gmail.com>
2025-01-25 22:35:19 +0000

    Completed H1 tests on Default Constructor. Continuing H2 tests.

--- src/smelter/lexical/binary/Original.java
+++ src/smelter/lexical/binary/Original.java
@@ -16,12 +16,16 @@ public class Original implements BinaryLexical<StickDouble>{
     private String interval;
     private ArrayList<StickDouble> stickList;
 
+    public final int H1_TOTAL_LEN;
+    private int h2_total_len;
+    private int h_total_len;
+
     //Binary Lexical H1
     private boolean[] h1_byid;
     private boolean[] h1_int;
     private boolean[] h1_ct_len;
     private boolean[] h1_data_len;
-    private boolean[] h1_h_gap;
+    private boolean[] h1_h_gap_len;
 
     private boolean[] h1_pw_len; //price (ohlc) bit length
     private boolean[] h1_vw_len; //volume bit length
@@ -33,13 +37,14 @@ public class Original implements BinaryLexical<StickDouble>{
     private boolean[] h2_sym_len;
     private boolean[] h2_sym;
     private boolean[] h2_data_ct;
+    private boolean[] h2_h_gap;
 
     //Translated Lexical H1
     private boolean t_h1_byid;
     private int t_h1_int;
     private int t_h1_data_len;
     private int t_h1_ct_len;
-    private byte t_h1_h_gap;
+    private byte t_h1_h_gap_len;
 
     private byte t_h1_pw_len; //price (ohlc)
     private byte t_h1_vw_len; //volume
@@ -49,33 +54,49 @@ public class Original implements BinaryLexical<StickDouble>{
 
     //Translated Lexical H2
     private byte t_h2_sym_len;
+    private int t_h2_data_ct;
 
     public Original(String symbol, String interval){
         this.symbol=symbol;
         this.interval=interval;
         stickList = new ArrayList<StickDouble>();
 
-        //Set the H1 sizes and some values
+        //Set ALL H1 bin sizes and some bin values
         h1_byid = new boolean[]{false};
         h1_int = new boolean[25];
         h1_ct_len = new boolean[26];
-        h1_data_len = new boolean[9];
-        h1_h_gap = new boolean[3];
+        h1_data_len = new boolean[9]; //Value set AFTER h1_(pv)(wf)_len values set
+        h1_h_gap_len = new boolean[3]; //Value set AFTER ALL h2 sizes
 
         h1_pw_len = new boolean[]{true,true,true,true,true};
-        h1_vw_len = new boolean[]{true,true,true,true,true};
+        h1_pf_len = new boolean[]{true,true,true,true};
 
-        h1_pf_len = new boolean[]{false,true,true,true,true};
-        h1_vf_len = new boolean[]{false,true,true,true,true};
+        h1_vw_len = new boolean[]{true,true,true,true,true};
+        h1_vf_len = new boolean[]{true,true,true,true};
+
+        H1_TOTAL_LEN = 
+            h1_byid.length +
+            h1_int.length +
+            h1_ct_len.length +
+            h1_data_len.length +
+            h1_h_gap_len.length +
+            h1_pw_len.length +
+            h1_pf_len.length +
+            h1_vw_len.length +
+            h1_vf_len.length;
 
         // Set translated and binary H1 values where applicable
+        t_h1_byid=h1_byid[0];
         t_h1_int=Integer.parseUnsignedInt(interval);
         BinaryTools.setUnsignedIntToBoolArray(t_h1_int,h1_int);
 
-        t_h1_ct_len = 1; // 1 bit required to represent 0.
+        t_h1_ct_len = 1; // 1 bit required to represent 0. Minimum bits required to represent current count
+        BinaryTools.setUnsignedIntToBoolArray(0,h1_ct_len); //Initially 0 Sticks
+
         t_h1_pw_len = (byte)BinaryTools.toUnsignedInt(h1_pw_len);
-        t_h1_vw_len = (byte)BinaryTools.toUnsignedInt(h1_vw_len);
         t_h1_pf_len = (byte)BinaryTools.toUnsignedInt(h1_pf_len);
+
+        t_h1_vw_len = (byte)BinaryTools.toUnsignedInt(h1_vw_len);
         t_h1_vf_len = (byte)BinaryTools.toUnsignedInt(h1_vf_len);
 
         t_h1_data_len = 44 + 4*(t_h1_pw_len + t_h1_pf_len) + t_h1_vw_len + t_h1_vf_len;
@@ -83,11 +104,30 @@ public class Original implements BinaryLexical<StickDouble>{
 
         //Set H2 translated values (these are already known)
         t_h2_sym_len = (byte)(symbol.length() << 3);
+        t_h2_data_ct = 0;
 
         //Set the H2 sizes and values
         h2_sym_len = BinaryTools.genBoolArrayFromUnsignedInt(t_h2_sym_len,(byte)7);
         h2_sym = BinaryTools.genBoolArrayFrom8BitCharString(symbol);
+
         h2_data_ct = new boolean[]{false};
+
+        //Set gap
+        int remainder = (H1_TOTAL_LEN + h2_sym_len.length + h2_sym.length + h2_data_ct.length)%8;
+        if(remainder != 0){
+            t_h1_h_gap_len = (byte)(8 - remainder);
+            BinaryTools.setUnsignedIntToBoolArray(t_h1_h_gap_len,h1_h_gap_len);
+            h2_h_gap = BinaryTools.genBoolArrayFromUnsignedInt(0,t_h1_h_gap_len);
+        }
+
+        //set remaining dependent initial values
+        h2_total_len =
+            h2_sym_len.length +
+            h2_sym.length + 
+            h2_data_ct.length +
+            h2_h_gap.length;
+
+        h_total_len = H1_TOTAL_LEN + h2_total_len;
     }
 
     // BinaryLexical Overrides
@@ -167,11 +207,11 @@ public class Original implements BinaryLexical<StickDouble>{
         h1[0] = BinaryTools.genClone(h1_byid); //h1_biid
         h1[1] = BinaryTools.genClone(h1_int); // h1_int
         h1[2] = BinaryTools.genClone(h1_ct_len); // h1_ct_len
-        h1[3] = BinaryTools.genClone(h1_data_len); // c
-        h1[4] = BinaryTools.genClone(h1_h_gap); // h1_h_gap
+        h1[3] = BinaryTools.genClone(h1_data_len); // h1_data_len
+        h1[4] = BinaryTools.genClone(h1_h_gap_len); // h1_h_gap_len
         h1[5] = BinaryTools.genClone(h1_pw_len); // h1_pw_len
-        h1[6] = BinaryTools.genClone(h1_vw_len); // h1_vw_len
-        h1[7] = BinaryTools.genClone(h1_pf_len); // h1_pf_len
+        h1[6] = BinaryTools.genClone(h1_pf_len); // h1_pf_len
+        h1[7] = BinaryTools.genClone(h1_vw_len); // h1_vw_len
         h1[8] = BinaryTools.genClone(h1_vf_len); // h1_vf_len
 
         return h1;
@@ -184,31 +224,33 @@ public class Original implements BinaryLexical<StickDouble>{
         h1[1] = h1_int;
         h1[2] = h1_ct_len;
         h1[3] = h1_data_len;
-        h1[4] = h1_h_gap;
+        h1[4] = h1_h_gap_len;
         h1[5] = h1_pw_len;
-        h1[6] = h1_vw_len;
-        h1[7] = h1_pf_len;
+        h1[6] = h1_pf_len;
+        h1[7] = h1_vw_len;
         h1[8] = h1_vf_len;
 
         return h1;
     }
 
     public boolean[][] genBinaryHeader2(){
-        boolean[][] h2=new boolean[3][];
+        boolean[][] h2=new boolean[4][];
 
         h2[0] = BinaryTools.genClone(h2_sym_len); //h2_sym_len
         h2[1] = BinaryTools.genClone(h2_sym); //h2_sym
         h2[2] = BinaryTools.genClone(h2_data_ct); //h2_data_ct
+        h2[3] = BinaryTools.genClone(h2_h_gap); //h2_h_gap
 
         return h2;
     }
 
     private boolean[][] genBinaryHeader2Ref(){
-        boolean[][] h2=new boolean[3][];
+        boolean[][] h2=new boolean[4][];
 
         h2[0] = h2_sym_len;
         h2[1] = h2_sym;
         h2[2] = h2_data_ct;
+        h2[3] = h2_h_gap;
 
         return h2;
     }
--- test/smelter/lexical/binary/OriginalTest.java
+++ test/smelter/lexical/binary/OriginalTest.java
@@ -27,13 +27,15 @@ public class OriginalTest{
             3 +  //h1_h_gap
             5 +  //h1_pw_len
             5 +  //h1_vw_len
-            5 +  //h1_pf_len
-            5;   //h1_vf_len
+            4 +  //h1_pf_len
+            4;   //h1_vf_len
         
         int data_len = 44 + 4*31 + 4*15 + 31 + 15; // UTC + 4*OHLC + V
 
         boolean[][] generatedHeader1 = first_lexical.genBinaryHeader1();
         boolean[] flatHeader1 = BinaryTools.genConcatenatedBoolArrays(generatedHeader1);
+        boolean[][] generatedHeader2 = first_lexical.genBinaryHeader2();
+        boolean[] flatHeader2 = BinaryTools.genConcatenatedBoolArrays(generatedHeader2);
 
         @Nested
         @DisplayName("Test Default Constructor Header 1 values independantly")
@@ -65,6 +67,57 @@ public class OriginalTest{
                 assertEquals(9,bin_h1_data_len.length);  // 9 bits to represent a single databoint
                 assertEquals(data_len,BinaryTools.toUnsignedInt(bin_h1_data_len));
             }
+
+            @Test
+            public void testDefault_Gap(){
+                boolean[] bin_h1_gap = generatedHeader1[4];
+                assertEquals(3,bin_h1_gap.length);
+                //Value not applicable yet...
+            }
+
+            @Test
+            public void testDefault_StickOHLC_Whole(){
+                boolean[] bin_h1_pw_len = generatedHeader1[5];
+                assertEquals(5,bin_h1_pw_len.length);
+                assertEquals(31,BinaryTools.toUnsignedInt(bin_h1_pw_len));
+            }
+
+            @Test
+            public void testDefault_StickOHLC_Fraction(){
+                boolean[] bin_h1_pf_len = generatedHeader1[6];
+                assertEquals(4,bin_h1_pf_len.length);
+                assertEquals(15,BinaryTools.toUnsignedInt(bin_h1_pf_len));
+            }
+            @Test
+            public void testDefault_StickVolume_Whole(){
+                boolean[] bin_h1_vw_len = generatedHeader1[7];
+                assertEquals(5,bin_h1_vw_len.length);
+                assertEquals(31,BinaryTools.toUnsignedInt(bin_h1_vw_len));
+            }
+
+            @Test
+            public void testDefault_StickVolume_Fraction(){
+                boolean[] bin_h1_vf_len = generatedHeader1[8];
+                assertEquals(4,bin_h1_vf_len.length);
+                assertEquals(15,BinaryTools.toUnsignedInt(bin_h1_vf_len));
+            }
+        }
+
+        @Nested
+        @DisplayName("Test Default Constructor Header 2 values independantly")
+        public class DefaultHeader2Test{
+            @Test
+            public void testDefault_SymbolBitLength(){
+                boolean[] bin_h2_sym_len = generatedHeader2[0];
+                assertEquals(6*8,BinaryTools.toUnsignedInt(bin_h2_sym_len)); //BTCUSD has 6 characters
+                assertEquals(7,bin_h2_sym_len.length); //firxed length of 7 bits
+            }
+
+            @Test
+            public void testDefault_Sybmbol(){
+                boolean[] bin_h2_sym = generatedHeader2[1];
+                assertEquals(6*8,bin_h2_sym.length); //BTCUSD has 6 characters
+            }
         }
 
         @Test
@@ -75,8 +128,9 @@ public class OriginalTest{
 
         @Test
         public void testDefaultHeader1(){
-            assertEquals(84,h1_len); //84 bits for h1
-            assertEquals(84,flatHeader1.length);
+            assertEquals(82,first_lexical.H1_TOTAL_LEN);
+            assertEquals(first_lexical.H1_TOTAL_LEN,flatHeader1.length);
+            assertEquals(flatHeader1.length,first_lexical.H1_TOTAL_LEN);
         }
     }
 }
