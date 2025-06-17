39ae2de397d0d5923c29e863083802df06cd57a2
iamlamb1988 <iamlamb1988@gmail.com>
2025-06-17 16:13:20 +0000

    Initial push. Minor change. Moving BinaryToolsTest to correct and respective package. This did not get moved upon original Tools transfer.

---
+++ test/smelter/filesmelter/OHLCV_BinaryLexicalSmallFileSmelterTest.java
@@ -0,0 +1,16 @@
+/**
+ * @author Bruce Lamb
+ * @since 17 JUN 2025
+ */
+package tradedatacorp.smelter.filesmelter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OHLCV_BinaryLexicalSmallFileSmelterTest{}
