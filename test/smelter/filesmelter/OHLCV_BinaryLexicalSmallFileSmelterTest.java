08336b657861dfd89a2380a1d147aa7cb64a037f
iamlamb1988 <iamlamb1988@gmail.com>
2025-06-17 16:46:02 +0000

    Added binary files to test/resources/smelter/filesmelter/ directory. Will be used to compare certain test cases.

--- test/smelter/filesmelter/OHLCV_BinaryLexicalSmallFileSmelterTest.java
+++ test/smelter/filesmelter/OHLCV_BinaryLexicalSmallFileSmelterTest.java
@@ -6,11 +6,24 @@ package tradedatacorp.smelter.filesmelter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
-public class OHLCV_BinaryLexicalSmallFileSmelterTest{}
+import java.nio.file.Path;
+
+public class OHLCV_BinaryLexicalSmallFileSmelterTest{
+    @Nested
+    @DisplayName("Super Simple Tests")
+    public class SuperSimpleTests(){
+        @Test
+        public void OneDataPointTest(@TempDir Path tempPath) throws Exception{
+            //TODO
+        }
+    }
+}
