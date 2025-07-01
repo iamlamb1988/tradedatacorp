93f2c978a327bea11c154002cb822c54fad1a27d
iamlamb1988 <iamlamb1988@gmail.com>
2025-07-01 11:34:20 +0000

    Redesigning the generic use warehouse structure and plan.

--- src/warehouse/Warehouse.java
+++ src/warehouse/Warehouse.java
@@ -1,6 +1,6 @@
 /**
  * @author Bruce Lamb
- * @since 21 MAY 2024
+ * @since 01 JUL 2025
  */
 
 package tradedatacorp.warehouse;
@@ -11,27 +11,10 @@ package tradedatacorp.warehouse;
  * This interface is intended to be used in conjuction with the helper Warehouse interfaces.
  */
 public interface Warehouse{
-	/**
-	 * Attempts to establish a connection to a warehouse.
-	 * @param credentials credentials required for persistent connection to warehouse.
-	 * @return an xml message tag, msg in the following format:
-	 * {@code
-	 * <msg>
-	 *   <status>[CONNECTION]</status>
-	 *   <warning>[WARNING]</warning>
-	 *   <error>[ERROR]</error>
-	 * </msg>
-	 * }
-	 * 
-	 * XML tag: msg will always have a status tag. Will only contain an error tag if an error has occured.
-	 * 
-	 * CONNECTION: "OK", "FAILED"
-	 * ERROR: Will state the nature of the error. Based on specific implementation
-	 */
-	public String connect(String credentials);
-
-	/**
-	 * @return "CONNECTED", "NONE", "STATELESS"
-	 */
-	public String connectStatus();
+    /**
+     * Attempts to establish a connection to a warehouse.
+     * @param credentials credentials required for persistent connection to warehouse.
+     * @return a message regarding state of connection attempt
+     */
+    public String connect(String credentials);
 }
--- src/warehouse/impl/OHLCV_BinaryWarehouse.java
+++ src/warehouse/impl/OHLCV_BinaryWarehouse.java
@@ -4,6 +4,17 @@
  */
 package tradedatacorp.warehouse;
 
-public class OHLCV_BinaryWarehouse{
-    
+/**
+ * A warehouse implementation for storing OHLCV candlestick data in a filesystem.
+ * This is a machine that is intended to constantly run but can be turned on or off without corruption.
+ * This instance will make use of {@link OHLCV_BinaryLexical} to super compress files. Will be able to quickly fetch data from
+ * files in it's compressed form utilizing unique super compressed format.
+ * Elaboration coming soon upon completion of JUnit tests.
+ */
+public class OHLCV_BinaryWarehouse implements Warehouse{
+    //Warehouse Overrides
+    @Override
+    public String connect(String credentials){
+        return null;
+    }
 }
