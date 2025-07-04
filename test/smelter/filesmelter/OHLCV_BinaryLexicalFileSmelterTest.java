6913ed4fbad1cf7c08cad6f5089eb0cdb35040a0
iamlamb1988 <iamlamb1988@gmail.com>
2025-07-04 19:32:42 +0000

    Refactored test cases to handle File smelting and String smelting separately.

--- test/smelter/filesmelter/OHLCV_BinaryLexicalFileSmelterTest.java
+++ test/smelter/filesmelter/OHLCV_BinaryLexicalFileSmelterTest.java
@@ -12,8 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.io.TempDir;
 
+import tradedatacorp.smelter.filesmelter.OHLCV_BinaryLexicalFileSmelter;
+import tradedatacorp.smelter.filesmelter.OHLCV_BinaryLexicalFileUnsmelter;
 import tradedatacorp.test.java.TestResourceFetcher;
 import tradedatacorp.smelter.filesmelter.OHLCV_ExpectedResourceValues;
 import tradedatacorp.smelter.lexical.binary.OHLCV_BinaryLexical;
@@ -24,69 +28,87 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class OHLCV_BinaryLexicalFileSmelterTest{
-    TestResourceFetcher testFileFetcher;
-    byte[] EXPECTED_ONEDATAPOINT_BYTE_VALUES;
-    byte[] EXPECTED_TWODATAPOINTS_BYTE_VALUES;
+    private static TestResourceFetcher testFileFetcher;
 
-    OHLCV_BinaryLexicalFileSmelterTest(){
+    private static Path expectedOneDatapointFile;
+    private static Path expectedTwoDatapointsFile;
+
+    private static byte[] EXPECTED_ONEDATAPOINT_BYTE_VALUES;
+    private static byte[] EXPECTED_TWODATAPOINTS_BYTE_VALUES;
+
+    @BeforeAll
+    static void initialize(){
         testFileFetcher = new TestResourceFetcher();
         EXPECTED_ONEDATAPOINT_BYTE_VALUES = OHLCV_ExpectedResourceValues.expectedOneDatapoint();
         EXPECTED_TWODATAPOINTS_BYTE_VALUES = OHLCV_ExpectedResourceValues.expectedTwoDatapoints();
+
+        expectedOneDatapointFile = testFileFetcher.getFilePath("smelter/filesmelter/OneDatapoint.brclmb");
+        expectedTwoDatapointsFile = testFileFetcher.getFilePath("smelter/filesmelter/TwoDatapoints.brclmb");
     }
 
-    @Test
-    void writeOneDatapoint(@TempDir Path tempDir){
-        OHLCV_BinaryLexicalFileSmelter smelter = new OHLCV_BinaryLexicalFileSmelter(OHLCV_BinaryLexical.genMiniLexical("TEST", 60, (byte)0));
-        OHLCV_BinaryLexicalFileUnsmelter reader = new OHLCV_BinaryLexicalFileUnsmelter();
+    @Nested
+    @DisplayName("Tests for smelter/filesmelter/OneDatapoint.brclmb")
+    class TestsForOneDatapoint{
+        private OHLCV_BinaryLexicalFileSmelter smelter;
 
-        Path testFilePath = testFileFetcher.getFilePath("smelter/filesmelter/OneDatapoint.brclmb");
-        Path resultFilePath = tempDir.resolve("result.brclmb");
+        @BeforeEach
+        void initializeSmelterBeforeEachTest(){
+            smelter = new OHLCV_BinaryLexicalFileSmelter(OHLCV_BinaryLexical.genMiniLexical("TEST", 60, (byte)0));
+            smelter.addData(new CandleStickFixedDouble(12, 4, 9, 2, 5, 10.5));
+        }
 
-        smelter.addData(new CandleStickFixedDouble(12, 4, 9, 2, 5, 10.5));
-        smelter.setTargetFile(resultFilePath);
-        smelter.smeltToFile();
-        String stringResult = smelter.smeltToString();
+        @Test
+        void writeOneDatapointToFile(@TempDir Path tmpDir){
+            Path resultOneDatapointFile = tmpDir.resolve("testResultOneDatapoint.brclmb");
+            smelter.setTargetFile(resultOneDatapointFile);
+            smelter.smeltToFile();
 
-        boolean ismatch = false;
-        try{ismatch = Files.mismatch(testFilePath,resultFilePath) == -1;
-        }catch(Exception err){err.printStackTrace();}
+            boolean ismatch = false;
+            try{ismatch = Files.mismatch(expectedOneDatapointFile,resultOneDatapointFile) == -1;}
+            catch(Exception err){err.printStackTrace();}
 
-        assertTrue(ismatch); //Gurantees no mismatch or errors on resultant file
-
-        //Additional checks, will ensure String resolution is also a match
-        assertEquals(21,EXPECTED_ONEDATAPOINT_BYTE_VALUES.length);
-        assertEquals(21,stringResult.length());
-
-        for(int i=0;i<stringResult.length(); ++i){
-            assertEquals(
-                EXPECTED_ONEDATAPOINT_BYTE_VALUES[i],
-                (byte)stringResult.charAt(i),
-                "byte mismatch: expected: "+EXPECTED_ONEDATAPOINT_BYTE_VALUES[i]+" but was "+(byte)stringResult.charAt(i)
-            );
+            assertTrue(ismatch);
         }
     }
 
-    @Test
-    void writeTwoDatapoints(@TempDir Path tempDir){
-        OHLCV_BinaryLexicalFileSmelter smelter = new OHLCV_BinaryLexicalFileSmelter(OHLCV_BinaryLexical.genMiniLexical("TEST", 60, (byte)0));
-        OHLCV_BinaryLexicalFileUnsmelter reader = new OHLCV_BinaryLexicalFileUnsmelter();
+     @Nested
+    @DisplayName("Tests for smelter/filesmelter/TwoDatapoints.brclmb")
+    class TestsForTwoDatapoints{
+        private OHLCV_BinaryLexicalFileSmelter smelter;
 
-        Path testFilePath = testFileFetcher.getFilePath("smelter/filesmelter/TwoDatapoints.brclmb");
-        Path resultFilePath = tempDir.resolve("result.brclmb");
+        @BeforeEach
+        void initializeSmelterBeforeEachTest(){
+            smelter = new OHLCV_BinaryLexicalFileSmelter(OHLCV_BinaryLexical.genMiniLexical("TEST", 60, (byte)0));
+            smelter.addData(new CandleStickFixedDouble(12, 4, 9, 2, 5, 10.5));
+            smelter.addData(new CandleStickFixedDouble(13, 4.1, 9.7, 2.2, 5, 15.6));
+        }
 
-        smelter.addData(new CandleStickFixedDouble(12, 4, 9, 2, 5, 10.5));
-        smelter.addData(new CandleStickFixedDouble(13, 4.1, 9.7, 2.2, 5, 15.6));
-        smelter.setTargetFile(resultFilePath);
-        smelter.smeltToFile();
+        @Test
+        void writeTwoDatapointsToFile(@TempDir Path tmpDir){
+            Path resultTwoDatapointsFile = tmpDir.resolve("testResultTwoDatapoints.brclmb");
+            smelter.setTargetFile(resultTwoDatapointsFile);
+            smelter.smeltToFile();
 
-        //CHANGE IN FUTURE
-        //Future issue: Should not test for exact file match, only need to check if rendered candles stick is exact match within the file
-        boolean ismatch = false;
-        try{
-            ismatch = Files.mismatch(testFilePath,resultFilePath) == -1;
-        }catch(Exception err){err.printStackTrace();}
+            boolean ismatch = false;
+            try{ismatch = Files.mismatch(expectedTwoDatapointsFile,resultTwoDatapointsFile) == -1;}
+            catch(Exception err){err.printStackTrace();}
 
-        assertTrue(ismatch,"Mismatch at index: ");
-        //END CHANGE IN FUTURE
+            assertTrue(ismatch);
+        }
+
+        @Test
+        void writeTwoDatapointsToString(){
+            String stringResult = smelter.smeltToString();
+            assertEquals(27,EXPECTED_TWODATAPOINTS_BYTE_VALUES.length);
+            assertEquals(27,stringResult.length());
+
+            for(int i=0;i<stringResult.length(); ++i){
+                assertEquals(
+                    EXPECTED_TWODATAPOINTS_BYTE_VALUES[i],
+                    (byte)stringResult.charAt(i),
+                    "byte mismatch: expected: "+EXPECTED_TWODATAPOINTS_BYTE_VALUES[i]+" but was "+(byte)stringResult.charAt(i) + (" at index: "+i)
+                );
+            }
+        }
     }
 }
